d2496df75ccfbf8f20c6c8dd1cea2888a78a860a
mjones2453 <mjones942676@gmail.com>
2025-03-16 20:07:34 -0400

    update constants.py, main.py, player.py

--- main.py
+++ main.py
@@ -6,23 +6,30 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
     
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
-   
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        
+
         dt = clock.tick(60) / 1000 # limit the framerate to 60 FPS
 
 if __name__ == "__main__":
